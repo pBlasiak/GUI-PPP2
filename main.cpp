d493ed8b3f4650548af66d1c4653675e70e108e9
pblasiak <przemyslaw.blasiak@pwr.edu.pl>
2025-11-27 18:40:17 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+﻿//
+// This is example code from Chapter 12.3 "A first example" of
+// "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
+//
+
+#include "Simple_window.h"    // get access to our window library
+#include "Graph.h"            // get access to our graphics library facilities
+
+//------------------------------------------------------------------------------
+
+//using namespace Graph_lib;   // our graphics facilities are in Graph_lib
+
+int main()
+try
+{
+	Graph_lib::Point tl(100, 100);     // top-left corner of our window
+
+	Simple_window win(tl, 600, 400, "Canvas");
+	// screen coordinate tl for top-left corner
+	// window size(600*400)
+	// title: Canvas
+	win.wait_for_button(); // Display!
+}
+catch (exception&) {
+	// some error reporting
+	return 1;
+}
+catch (...) {
+	// some more error reporting
+	return 2;
+}
+
+
+//------------------------------------------------------------------------------
