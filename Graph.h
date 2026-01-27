ae1b5a31e56d0d1735463254be8a50e2a704f775
pblasiak <przemyslaw.blasiak@pwr.edu.pl>
2026-01-27 10:15:48 +0100

    Dodano rzutowanie na int w Shape::lcolor

--- Graph.h
+++ Graph.h
@@ -157,7 +157,7 @@ public:
 	Shape& operator=(const Shape&) = delete;
 private:
 	vector<Point> points;	// not used by all shapes
-	Color lcolor {fl_color()};
+	Color lcolor {static_cast<int>(fl_color())};
 	Line_style ls {0};
 	Color fcolor {Color::invisible};
 
